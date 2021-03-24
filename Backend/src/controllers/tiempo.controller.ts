b00c5ec46ab53c341aa7c1f7efba81822808b425
cristianhu96 <cristianhu96@gmail.com>
2021-03-24 14:06:07 -0300

    Listo Backend

--- Backend/src/controllers/tiempo.controller.ts
+++ Backend/src/controllers/tiempo.controller.ts
@@ -6,6 +6,9 @@ export class TiempoController {
   constructor(  @inject('services.RestSample')
   private restSampleService: RestSampleService) {}
 
+  /**
+   * Obtiene la ciudad segun el string concatenado
+   */
    @get('/rest/{city}')
    getone(@param.path.string('city') city: string) {
       return this.restSampleService.getrestdata(city);
--- Backend/src/datasources/restsample.datasource.ts
+++ Backend/src/datasources/restsample.datasource.ts
@@ -2,7 +2,12 @@ import {juggler} from '@loopback/service-proxy';
 import * as config from '../datasources/rest.config.json';
 
 export class RestSampleDataSource extends juggler.DataSource {
+   
+   /**
+   * DataSource que nos permita conectarnos con la Api Rest
+   */
    static dataSourceName = 'restsample';
+
    constructor(dsConfig: object = config) {
       super(dsConfig);
    }
